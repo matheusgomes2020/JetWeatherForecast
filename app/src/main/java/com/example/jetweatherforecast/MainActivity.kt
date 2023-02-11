89a8406828931d389ff47f5ac9dca0d921cde001
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-02-11 15:51:22 -0300

    Showing the SplashScreen.

--- app/src/main/java/com/example/jetweatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/jetweatherforecast/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.jetweatherforecast
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetweatherforecast.navigation.WeatherNavigation
 import com.example.jetweatherforecast.ui.theme.JetWeatherForecastTheme
 import dagger.hilt.EntryPoint
 
@@ -18,28 +22,38 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetWeatherForecastTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
+            WeatherApp()
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun WeatherApp() {
+
+    JetWeatherForecastTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            color = MaterialTheme.colors.background,
+            modifier = Modifier.fillMaxSize()) {
+
+            Column(verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+
+                WeatherNavigation()
+
+            }
+
+        }
+    }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetWeatherForecastTheme {
-        Greeting("Android")
     }
 }
