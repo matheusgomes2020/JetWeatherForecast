25b6eb540f7fafb5b4a675f02175eb99d062d560
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-02-15 17:42:06 -0300

    .

--- app/src/main/java/com/example/jetweatherforecast/utils/Constants.kt
+++ app/src/main/java/com/example/jetweatherforecast/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.example.jetweatherforecast.utils
 object Constants {
 
     const val BASE_URL = "https://api.openweathermap.org/"
-    const val API_KEY = ""
+    const val API_KEY = "***"
 
 }
