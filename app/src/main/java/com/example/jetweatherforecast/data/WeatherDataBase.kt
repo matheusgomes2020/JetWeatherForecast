886c04de96b22b27fe07117c0940fd7e04781d44
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-02-21 15:53:21 -0300

    Setting up Room Database and Repository.

---
+++ app/src/main/java/com/example/jetweatherforecast/data/WeatherDataBase.kt
@@ -0,0 +1,12 @@
+package com.example.jetweatherforecast.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.jetweatherforecast.model.Favorite
+
+@Database( entities = [Favorite::class], version = 1, exportSchema = false )
+abstract class WeatherDataBase: RoomDatabase() {
+
+    abstract fun weatherDao(): WeatherDao
+
+}
--- app/src/main/java/com/example/jetweatherforecast/di/AppModule.kt
+++ app/src/main/java/com/example/jetweatherforecast/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.example.jetweatherforecast.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.jetweatherforecast.data.WeatherDataBase
+import com.example.jetweatherforecast.data.WeatherDao
 import com.example.jetweatherforecast.network.WeatherApi
 import com.example.jetweatherforecast.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -13,6 +18,21 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase( @ApplicationContext context: Context                                                                  ): WeatherDataBase
+    = Room.databaseBuilder(
+        context,
+        WeatherDataBase::class.java,
+        "weather_database" )
+        .fallbackToDestructiveMigration()
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideWeatherDao( weatherDataBase: WeatherDataBase ):
+            WeatherDao = weatherDataBase.weatherDao()
 
     @Provides
     @Singleton
